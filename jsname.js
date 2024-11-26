a9bebfa08d937caa3ee71dd2e0304816b38f7db1
DrVickie <116500226+drvickie@users.noreply.github.com>
2024-11-26 14:13:16 +0000

    Student's javascript introduction

---
+++ jsname.js
@@ -0,0 +1,69 @@
+/*
+  // Your JavaScript goes here!
+  console.log("Hello, Worldly people!")
+  let name = "Peter";
+let surname = "Ade";
+
+console.log(name);
+console.log(surname);
+const pi = 3.14;
+console.log(pi); // What will be output?
+
+let counter = 2;
+counter++;        // works the same as counter = counter + 1, but is shorter
+alert( counter ); // 3
+*/
+
+
+//Example if/elseif/else
+/*let userName = prompt("Who's there?", '');
+
+if (userName === 'Doctor') {
+    let pass = prompt('Password?', '');
+
+    if (pass === 'TheMD') {
+      alert( 'Welcome!' );
+    } else if (pass === '' || pass === null) {
+      alert( 'Canceled' );
+    } else {
+      alert( 'Wrong password' );
+    }
+
+} else if (userName === '' || userName === null) {
+  alert( 'Canceled' );
+} else {
+  alert( "I don't know you" );
+}
+
+
+/*
+    let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
+    for (let i = 1; i <= answer; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+          window.alert("FizzBuzz");
+        } else if (i % 3 === 0) {
+          window.alert("Fizz");
+        } else if (i % 5 === 0) {
+          window.alert("Buzz");
+        } else {
+          window.alert(i);
+        }
+      }
+*/
+
+/*same
+let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
+
+for (let i = 1; i <= answer; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
+*/
+
